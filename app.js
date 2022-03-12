e48d71c2fc4bd5060a2ea2fae664ba2aca58f629
Desmond Lim <dkelim1@gmail.com>
2022-03-12 10:47:44 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const mysql = require('mysql');
+ 
+// parse application/json
+app.use(bodyParser.json());
+ 
+//create database connection
+const conn = mysql.createConnection({
+  host: 'uat-mysqld',
+  user: 'mysql-user',
+  password: 'root1234',
+  database: 'hello_world'
+});
+ 
+//connect to database
+conn.connect((err) =>{
+  if(err) throw err;
+  console.log('Mysql Connected...');
+});
+ 
+//show all products
+app.get('/',(req, res) => {
+  let sql = "SELECT * FROM messages";
+  let query = conn.query(sql, (err, results) => {
+    if(err) throw err;
+//    res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+    res.send(results);
+  });
+});
+  
+//Server listening
+app.listen(80,() =>{
+  console.log('Server started on port 80...');
+});
